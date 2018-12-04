996659d11257ea1c93f1beefff74eaf477176477
yym <1614527443@qq.com>
2018-12-04 21:40:48 +0800

    使用 chai.spy 监听函数

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ new Vue({
 // 单元测试
 import chai from 'chai'
 const expect = chai.expect
+import spies from 'chai-spies'
+chai.use(spies)
 // 1. 
 {
     const Constructor = Vue.extend(Button)
@@ -88,7 +90,6 @@ const expect = chai.expect
 }
 // 测试 click 事件
 {
-    // mock
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
         propsData: {
@@ -96,10 +97,10 @@ const expect = chai.expect
         }
     })
     vm.$mount()
-    vm.$on('click', function () {
-        expect(1).to.eq(1)
-    })
+    let spy = chai.spy(function(){})
+    vm.$on('click', spy)
     // 希望这个函数被执行
     let button = vm.$el
     button.click()
+    expect(spy).to.have.been.called()
 }
