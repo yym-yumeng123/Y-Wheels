7210a4fe694eaedbaf3e556a0aeef465e89f5701
yym <1614527443@qq.com>
2018-12-07 14:22:07 +0800

    增加 offset 偏移量

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]">
         <slot></slot>
     </div>
 </template>
@@ -11,6 +11,9 @@ export default {
         span: {
             type: [Number, String]
         },
+        offset: {
+            type: [Number, String]
+        }
     }
 }
 </script>
@@ -27,6 +30,12 @@ export default {
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n / 24) * 100%
+            } 
+        }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%
             } 
         }
     }
