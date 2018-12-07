f62d843ca512f137574f8f6491d40676b8dbb829
yym <1614527443@qq.com>
2018-12-07 11:09:54 +0800

    input props 测试用例通过

--- src/app.js
+++ src/app.js
@@ -15,6 +15,11 @@ new Vue({
     data: {
         loading1: true,
         loading2: false
+    },
+    methods: {
+        inputChange(e) {
+            console.log(e)
+        }
     }
 })
 
---
+++ test/input.test.js
@@ -0,0 +1,67 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+    it('存在.', () => {
+        expect(Input).to.exist  // 期望 input 存在
+    })
+
+    describe('props', () => {
+        const Constructor = Vue.extend(Input)
+        let vm
+        afterEach(() => {
+            vm.$destroy()
+        })
+        
+        it('接受 value', () => {
+            vm = new Constructor({
+                propsData:{
+                    value: '1234'
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.value).to.equal('1234')
+        })
+        it('接收 disabled', () => {
+            vm = new Constructor({
+                propsData:{
+                    disabled: true
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.disabled).to.equal(true)
+        })
+        it('接收 readonly', () => {
+            vm = new Constructor({
+                propsData:{
+                    readonly: true
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.readOnly).to.equal(true)
+        })
+        it('接收 error', () => {
+            vm = new Constructor({
+                propsData:{
+                    error: '你错了'
+                }
+            }).$mount()
+            const useElement = vm.$el.querySelector('use')
+            expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+            const errorMessage = vm.$el.querySelector('.errorMessage')
+            expect(errorMessage.innerText).to.equal('你错了')
+        })
+    })
+
+    describe('事件', () => {
+        it('支持 change 事件', () => {
+
+        })
+    })
+
+})
+
