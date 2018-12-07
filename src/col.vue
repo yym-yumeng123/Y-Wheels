27f90cb46873c678e03558ec54c9f3d6c5382754
yym <1614527443@qq.com>
2018-12-07 14:11:35 +0800

    完成 col 和 row 基本雏形

---
+++ src/col.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ElementCol',
+    props: {
+        span: {
+            type: [Number, String]
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .col {
+        height: 100px;
+        background: #ccc;
+        width: 50%;
+        border: 1px solid #000;
+        // flex-shrink: 0;
+        // for循环width
+        $class-prefix: col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width: ($n / 24) * 100%
+            } 
+        }
+    }
+</style>
+
