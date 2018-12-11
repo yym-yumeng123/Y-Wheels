e6d310f561120f9d296e0d71c97ed59ab0654335
yym <1614527443@qq.com>
2018-12-11 20:12:09 +0800

    布局基本组件完成

--- src/layout.vue
+++ src/layout.vue
@@ -1,11 +1,40 @@
 <template>
-    <div class="layout">
-        <slot></slot>
-    </div>
+  <div class="layout" :class="layoutClass">
+    <slot></slot>
+  </div>
 </template>
 
+<script>
+export default {
+	name: 'ElementLayout',
+	data() {
+		return {
+			layoutClass: {
+				hasSider: false
+			}
+		}
+	},
+	mounted () {
+		console.log('mounted')
+		console.log(this.$children)
+		this.$children.forEach((vm) => {
+			if(vm.$options.name === 'ElementSider') {
+				this.layoutClass.hasSider = true
+			}
+		})
+	}
+}
+</script>
+
+
 <style lang="scss" scoped>
-    .layout {
-        
-    }
+.layout {
+	display: flex;
+	flex-grow: 1;
+	flex-direction: column;
+	border: 1px solid red;
+	&.hasSider {
+		flex-direction: row;
+	}
+}
 </style>
---
+++ src/sider.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ElementSider'
+}
+</script>
+
+
+<style lang="scss" scoped>
+    .sider {
+        
+    }
+</style>
