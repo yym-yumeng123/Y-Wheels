ef35833257b6affece2ab5399ef7310bc9cf1431
yym <1614527443@qq.com>
2018-12-14 13:59:00 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display:inline-block">
       <slot></slot>
     </span>
   </div>
@@ -16,29 +16,37 @@
       return {visible: false}
     },
     methods: {
+			// 定位内容
       positionContent () {
         document.body.appendChild(this.$refs.contentWrapper)
         let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
         this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
         this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-      },
+			},
+			// 点击事件
       onClickDocument (e) {
         if (this.$refs.popover &&
           (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+				) { return }
+				if (this.$refs.contentWrapper &&
+          (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
         ) { return }
         this.close()
-      },
+			},
+			// 开启
       open () {
         this.visible = true
         this.$nextTick(() => {
           this.positionContent()
           document.addEventListener('click', this.onClickDocument)
         })
-      },
+			},
+			// 关闭
       close () {
         this.visible = false
         document.removeEventListener('click', this.onClickDocument)
-      },
+			},
+			// 点击是按钮就切换, 如果不是,就不管
       onClick (event) {
         if (this.$refs.triggerWrapper.contains(event.target)) {
           if (this.visible === true) {
@@ -53,6 +61,8 @@
 </script>
 
 <style scoped lang="scss">
+	$border-radius: 4px;
+	$border-color: #333;
   .popover {
     display: inline-block;
     vertical-align: top;
@@ -60,8 +70,32 @@
   }
   .content-wrapper {
     position: absolute;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    border: 1px solid $border-color;
+		border-radius: $border-radius;
+    filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+    background: #fff;
     transform: translateY(-100%);
+		margin-top: -10px;
+		padding: .5em 1em;
+		max-width: 20em;
+		// 字母过长换行
+		word-break: break-all;
+    &::before, &::after {
+      content: '';
+      display: block;
+      border: 10px solid transparent;
+      width: 0;
+      height: 0;
+      position: absolute;
+      left: 10px;
+    }
+    &::before {
+      border-top-color: #000;
+      top: 100%;
+    }
+    &::after {
+      border-top-color: #fff;
+      top: calc(100% - 1px);
+    }
 	}
 </style>
