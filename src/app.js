cd1f514ecc1f9de24992c33ddb522d485c3f2188
yym <1614527443@qq.com>
2018-12-15 11:02:38 +0800

    手风琴collapse效果和功能基本完成

--- src/app.js
+++ src/app.js
@@ -25,6 +25,9 @@ import TabsPane from './tabs-pane'
 
 import Popover from './popover'
 
+import Collapse from './collapse'
+import CollapseItem from './collapse-item'
+
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
@@ -50,6 +53,9 @@ Vue.component('g-tabs-item', TabsItem)
 Vue.component('g-tabs-pane', TabsPane)
 
 Vue.component('g-popover', Popover)
+
+Vue.component('g-collapse', Collapse)
+Vue.component('g-collapse-item', CollapseItem)
 
 new Vue({
     el: '#app',
---
+++ src/collapse.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ElementCollapse',
+}
+</script>
+
+<style lang="scss" scoped>
+    $grey: #ddd;
+    $border-radius: 4px;
+    .collapse {
+        border: 1px solid $grey;
+        border-radius: $border-radius;
+        // border-bottom: none;
+    }
+</style>
+
+
