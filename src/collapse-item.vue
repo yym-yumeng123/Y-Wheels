88130f547002957edfc604c5b8f4ec3f4abe9035
yym <1614527443@qq.com>
2018-12-15 11:23:00 +0800

    效果不太好的single 实现只展开一个colpaseItem

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="collapseItem">
-        <div class="title" @click="trigger">
+        <div class="title" @click="toggle">
             {{ title }}
         </div>
         <div class="content" v-if="open">
@@ -18,15 +18,31 @@ export default {
             required: true
         }
     },
+    inject: ['eventBus'],
     data () {
         return {
             open: false
         }
     },
     methods: {
-        trigger () {
-            this.open = !this.open
+        toggle () {
+            if(this.open) {
+                this.open= false
+            } else {
+                this.open= true
+                this.eventBus && this.eventBus.$emit('update:selected', this)
+            }
+        },
+        close () {
+            this.open = false
         }
+    },
+    mounted () {
+        this.eventBus && this.eventBus.$on('update:selected', (vm) => {
+            if(vm !== this) {
+                this.close()
+            }
+        })
     }
 }
 </script>
