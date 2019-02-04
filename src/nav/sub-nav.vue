e69d395f2cd59e4ea3f8f3f591d25ad501fa787d
yym-yumeng123 <18026493873@163.com>
2019-02-04 15:05:26 +0800

    使用 v-show 和 v-if 的区别

--- src/nav/sub-nav.vue
+++ src/nav/sub-nav.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="y-sub-nav">
-        <span class="y-sub-nav-title">
+        <span class="y-sub-nav-title" @click="onClick">
             <slot name="title"></slot>
         </span>
-        <div class="y-sub-nav-popover">
+        <!-- v-if 让元素出现 / 不出现在页面 v-show 加 style 一直在页面-->
+        <!-- v-if true / created false / destroy , v-show 只改 style -->
+        <div class="y-sub-nav-popover" v-show="open">
             <slot></slot>
         </div>
     </div>
@@ -12,13 +14,27 @@
 <script>
 export default {
     name: 'YSubNav',
+    data () {
+        return {
+            open: false
+        }
+    },
+    methods: {
+        onClick () {
+            this.open = !this.open
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
     .y-sub-nav {
         position: relative;
-        padding: 10px 20px;
+        > span {
+            display: inline-block;
+            vertical-align: top;
+            padding: 10px 20px;
+        }
         &-popover {
             position: absolute;
             top: 100%;
