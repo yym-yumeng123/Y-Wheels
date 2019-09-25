85898fbe4083b024224a3f94a71a6be18a20a069
yym-yumeng123 <18026493873@163.com>
2019-09-25 23:26:11 +0800

    feat(table): 创建table, 给 columns data 两个 props

---
+++ src/table/table.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		Table
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'YTable',
+	props: {
+		columns: {
+			type: Array,
+			required: true
+		},
+		data: {
+			type: Array,
+			required: true
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
