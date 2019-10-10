88f26a7a8ebd54bd4cf6fb566c47e339ab71ed68
yym-yumeng123 <18026493873@163.com>
2019-10-10 23:25:34 +0800

    fix: 修改 karam 测试器的问题

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,20 @@
-var webpackConfig = require('@vue/cli-service/webpack.config.js')
+// karma 读取 webpack 里面的配置
+const webpackConfig = require('@vue/cli-service/webpack.config.js')
 
 module.exports = function (config) {
-    config.set({
-        frameworks: ['mocha'],
+	config.set({
+		frameworks: ['mocha'],
+		// 需要跑的文件地址
+		files: ['tests/**/*.spec.js'],
 
-        files: ['tests/**/*.spec.js'],
+		preprocessors: {
+			'**/*.spec.js': ['webpack', 'sourcemap']
+		},
 
-        preprocessors: {
-            '**/*.spec.js': ['webpack', 'sourcemap']
-        },
+		webpack: webpackConfig,
 
-        webpack: webpackConfig,
-
-        reporters: ['spec'],
-
-        browsers: ['Chrome']
-    })
+		reporters: ['spec'],
+		// 运行的测试器, 无头 Chrome
+		browsers: ['ChromeHeadless']
+	})
 }
--- tests/unit/nav.spec.js
+++ tests/unit/nav.spec.js
@@ -1,7 +1,7 @@
-import chai, {expect} from 'chai'
+import chai, { expect } from 'chai'
 import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
-import {shallowMount, mount} from '@vue/test-utils'
+import { shallowMount, mount } from '@vue/test-utils'
 import Nav from '../../src/nav/nav.vue'
 import NavItem from '../../src/nav/nav-item.vue'
 import SubNav from '../../src/nav/sub-nav.vue'
@@ -10,60 +10,60 @@ import Vue from 'vue'
 chai.use(sinonChai)
 
 describe('nav.vue', () => {
-    it('存在.', () => {
-        expect(Nav).to.exist
-    })
-    it('支持selected 属性', (done) => {
-        Vue.component('y-nav-item', NavItem)
-        Vue.component('y-sub-nav', SubNav)
-        const wrapper = mount(Nav, {
-            propsData : {
-                selected: 'home'
-            },
-            slots: {
-                default: `
-                    <y-nav-item name="home">
-                        <a href="https://www.baidu.com" target="_blank">首页</a>
-                    </y-nav-item>
-                    <y-sub-nav name="about">
-                        <template slot="title">关于</template>
-                        <y-nav-item name="culture">企业文化</y-nav-item>
-                    </y-sub-nav>
-                `
-            }
-        })
-        setTimeout(()=> {
-            expect(wrapper.find('[data-name="home"].active').exists()).to.be.true
-            done()
-        })
-    })
-    it('会触发 update:selected 事件.', (done) => {
-        Vue.component('y-nav-item', NavItem)
-        Vue.component('y-sub-nav', SubNav)
-        const callback = sinon.fake();
-        const wrapper = mount(Nav, {
-            propsData : {
-                selected: 'home'
-            },
-            slots: {
-                default: `
-                    <y-nav-item name="home">
-                        <a href="https://www.baidu.com" target="_blank">首页</a>
-                    </y-nav-item>
-                    <y-sub-nav name="about">
-                        <template slot="title">关于</template>
-                        <y-nav-item name="culture">企业文化</y-nav-item>
-                        <y-nav-item name="developers">开发团队</y-nav-item>
-                    </y-sub-nav>
-                `
-            },
-            listeners: {
-                'update:selected': callback
-            }
-        })
-        wrapper.find('[data-name="developers"]').trigger('click')
-        expect(callback).to.have.been.calledWith('developers')
-        done()
-    })
+	it('存在.', () => {
+		expect(Nav).to.exist
+	})
+	it('支持selected 属性', (done) => {
+		Vue.component('y-nav-item', NavItem)
+		Vue.component('y-sub-nav', SubNav)
+		const wrapper = mount(Nav, {
+			propsData: {
+				selected: 'home'
+			},
+			slots: {
+				default: `
+					<y-nav-item name="home">
+							<a href="https://www.baidu.com" target="_blank">首页</a>
+					</y-nav-item>
+					<y-sub-nav name="about">
+							<template slot="title">关于</template>
+							<y-nav-item name="culture">企业文化</y-nav-item>
+					</y-sub-nav>
+				`
+			}
+		})
+		setTimeout(() => {
+			expect(wrapper.find('[data-name="home"].active').exists()).to.be.true
+			done()
+		})
+	})
+	it('会触发 update:selected 事件.', (done) => {
+		Vue.component('y-nav-item', NavItem)
+		Vue.component('y-sub-nav', SubNav)
+		const callback = sinon.fake();
+		const wrapper = mount(Nav, {
+			propsData: {
+				selected: 'home'
+			},
+			slots: {
+				default: `
+					<y-nav-item name="home">
+							<a href="https://www.baidu.com" target="_blank">首页</a>
+					</y-nav-item>
+					<y-sub-nav name="about">
+							<template slot="title">关于</template>
+							<y-nav-item name="culture">企业文化</y-nav-item>
+							<y-nav-item name="developers">开发团队</y-nav-item>
+					</y-sub-nav>
+				`
+			},
+			listeners: {
+				'update:selected': callback
+			}
+		})
+		wrapper.find('[data-name="developers"]').trigger('click')
+		expect(callback).to.have.been.calledWith('developers')
+		done()
+	})
 })
 
